75ba63041a099da8a32b917c4c7d395cd2351df9
t.jahari <tristan.jahari@edu.tbz.ch>
2024-11-16 14:23:30 +0100

    Created Project for Q1

---
+++ Q/Q1/QuizApp/src/Question.java
@@ -0,0 +1,15 @@
+public class Question {
+    String Question;
+    String Answer1;
+    String Answer2;
+    String Answer3;
+    String Answer4;
+    public Question(String Question, String Answer1, String Answer2, String Answer3, String Answer4)
+    {
+        this.Question = Question;
+        this.Answer1 = Answer1;
+        this.Answer2 = Answer2;
+        this.Answer3 = Answer3;
+        this.Answer4 = Answer4;
+    }
+}
