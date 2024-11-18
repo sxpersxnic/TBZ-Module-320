1f9fb255078744d9f97bb6589da5b23aef207053
t.jahari <tristan.jahari@edu.tbz.ch>
2024-11-18 21:24:40 +0100

    Continued work on QuizApp

--- Q/Q1/QuizApp/src/Main.java
+++ Q/Q1/QuizApp/src/Main.java
@@ -1,11 +1,31 @@
-public class Main {
-    public static void main(String[] args)
-    {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        Scanner QuestionScanner = new Scanner(new File ("questions.txt"))
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner QuestionScanner = new Scanner (new File("C:\\Users\\trist\\git\\m320\\Q\\Q1\\QuizApp\\src\\questions.txt"));
         ArrayList<Question> questionList = new ArrayList<>();
 
-        System.out.println("Willkommen zum Quizgame!");
+        while(QuestionScanner.hasNextLine())
+        {
+            String line = QuestionScanner.nextLine();
+            String [] parts = line.split(",");
+            Question nextQuestion = new Question(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
+            questionList.add(nextQuestion);
+        }
+        for(Question q : questionList)
+        {
+            System.out.println(q.Question);
+            System.out.println(q.Answer1);
+            System.out.println(q.Answer2);
+            System.out.println(q.Answer3);
+            System.out.println(q.Answer4);
+            System.out.println(q.CorrectAnswer);
+        }
+
 
     }
 }
--- Q/Q1/QuizApp/src/Question.java
+++ Q/Q1/QuizApp/src/Question.java
@@ -4,12 +4,14 @@ public class Question {
     String Answer2;
     String Answer3;
     String Answer4;
-    public Question(String Question, String Answer1, String Answer2, String Answer3, String Answer4)
+    String CorrectAnswer;
+    public Question(String Question, String Answer1, String Answer2, String Answer3, String Answer4, String CorrectAnswer)
     {
         this.Question = Question;
         this.Answer1 = Answer1;
         this.Answer2 = Answer2;
         this.Answer3 = Answer3;
         this.Answer4 = Answer4;
+        this.CorrectAnswer = CorrectAnswer;
     }
 }
